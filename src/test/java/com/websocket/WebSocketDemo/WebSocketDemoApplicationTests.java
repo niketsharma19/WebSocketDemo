0c9084d136dee7f73cd109652cf39802a2acee25
Niket Sharma <niketsharma22@gmail.com>
2023-05-03 22:27:47 +0530

    Initial code for Spring Boot WebSocket Demo

---
+++ src/test/java/com/websocket/WebSocketDemo/WebSocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.WebSocketDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
