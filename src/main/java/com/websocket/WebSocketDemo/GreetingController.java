389eac280cb2750fb3030202bb031772a4e3b442
Niket Sharma <niketsharma22@gmail.com>
2023-05-06 16:55:09 +0530

    Updated code to send message from server to client

--- src/main/java/com/websocket/WebSocketDemo/GreetingController.java
+++ src/main/java/com/websocket/WebSocketDemo/GreetingController.java
@@ -1,18 +1,27 @@
 package com.websocket.WebSocketDemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
 
+	@Autowired
+	private SimpMessageSendingOperations messagingTemplate;
+
 	@MessageMapping("/hello")
-	@SendTo("/topic/greetings")
+//	@SendTo("/topic/greetings")
 	public Greeting greeting(HelloMessage message) throws Exception {
 		Thread.sleep(1000); // simulated delay
 		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}
+	
+//  Use the below method to send message to the client without receiving any message first
+	public void extracted(HelloMessage message) {
+		messagingTemplate.convertAndSend("/topic/greetings", new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!"));
 	}
 
 }
