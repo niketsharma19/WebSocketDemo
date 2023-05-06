045c4d2ed307b4e04871b30412f0b09569be73af
Niket Sharma <niketsharma22@gmail.com>
2023-05-06 16:55:56 +0530

    Added sendTo annotation

--- src/main/java/com/websocket/WebSocketDemo/GreetingController.java
+++ src/main/java/com/websocket/WebSocketDemo/GreetingController.java
@@ -2,6 +2,7 @@ package com.websocket.WebSocketDemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
@@ -13,7 +14,7 @@ public class GreetingController {
 	private SimpMessageSendingOperations messagingTemplate;
 
 	@MessageMapping("/hello")
-//	@SendTo("/topic/greetings")
+	@SendTo("/topic/greetings")
 	public Greeting greeting(HelloMessage message) throws Exception {
 		Thread.sleep(1000); // simulated delay
 		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
